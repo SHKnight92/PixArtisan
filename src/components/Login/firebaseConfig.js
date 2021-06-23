233139a21219bc1f292efc39a0eedf5ff7a2e78f
Saddam hossain knight <knight.ru.bd.com>
2021-06-23 07:16:54 +0600

    first commit

---
+++ src/components/Login/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAU0dRLHM3uwMRW7KKkRiB7mSvpUInsPsA",
+  authDomain: "pixartisan.firebaseapp.com",
+  projectId: "pixartisan",
+  storageBucket: "pixartisan.appspot.com",
+  messagingSenderId: "1091025391567",
+  appId: "1:1091025391567:web:fa4542edf864f537f9ccba",
+};
+
+export default firebaseConfig;
